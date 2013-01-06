a3ce0f2be3f88f188501b99817e101146f0adee6
Victor Koronen <koronen@kth.se>
2013-01-07 00:03:47 +0100

    Add zipfsong skeleton

---
+++ zipfsong/zipfsong.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+
+import sys
+
+class ZipfSong:
+    pass
+
+def main():
+    for line in sys.stdin:
+        print line
+
+if __name__ == '__main__':
+    main()
+
---
+++ zipfsong/zipfsong_test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import unittest
+from zipfsong import ZipfSong
+
+class TestZipfSong(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
+
