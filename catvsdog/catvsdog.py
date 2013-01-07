a4451ba600248603dd0d365ba3d188df14517089
Victor Koronen <koronen@kth.se>
2013-01-07 20:51:00 +0100

    Add catvsdog skeleton

---
+++ catvsdog/catvsdog.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+class CatVsDog:
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
---
+++ catvsdog/catvsdog_test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+from catvsdog import CatVsDog
+
+class TestCatVsDog(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
+
