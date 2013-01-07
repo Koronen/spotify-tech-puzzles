188ed5b4cd652739a2dc2330cd16be9b2959fd6b
Victor Koronen <koronen@kth.se>
2013-01-07 20:40:13 +0100

    Add initial implementation of zipfsong

--- zipfsong/zipfsong.py
+++ zipfsong/zipfsong.py
@@ -1,13 +1,49 @@
-#!/usr/bin/env
+#!/usr/bin/env python
 
 import sys
 
 class ZipfSong:
-    pass
+    def __init__(self):
+        self.songs = []
+        self.totalPlayCount = 0
+
+    def addSong(self, i, f, s):
+        self.songs.append((i, f, s))
+        self.totalPlayCount += f
+
+    def topQualitySongs(self, m):
+        if m <= 0:
+            return []
+
+        return sorted(self.songs, cmp=self.compareSongs)[:m]
+
+    def compareSongs(self, x, y):
+        qd = cmp(self.songQuality(*y), self.songQuality(*x))
+        if qd != 0:
+            return qd
+        else:
+            return cmp(self.songIndex(*x), self.songIndex(*y))
+
+    def songIndex(self, i, f, s):
+        return i
+
+    def songQuality(self, i, f, s):
+        return 1.0 * 2**(i+1) * f / self.totalPlayCount
+
+    def songTitle(self, i, f, s):
+        return s
+
 
 def main():
-    for line in sys.stdin:
-        print line
+    (n, m) = [int(i) for i in sys.stdin.readline().split(" ")]
+
+    zs = ZipfSong()
+    for i in range(n):
+        (fi, si) = sys.stdin.readline().split(" ", 1)
+        zs.addSong(i, int(fi), si.strip())
+
+    for s in map(lambda s: zs.songTitle(*s), zs.topQualitySongs(m)):
+        print s
 
 if __name__ == '__main__':
     main()
--- zipfsong/zipfsong_test.py
+++ zipfsong/zipfsong_test.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 
 import unittest
+
 from zipfsong import ZipfSong
 
 class TestZipfSong(unittest.TestCase):
-    pass
+    def test_topQualitySongs_no_songs_added(self):
+        zs = ZipfSong()
+        self.assertEquals(zs.topQualitySongs(3), [])
+
+    def test_topQualitySongs_no_songs_requested(self):
+        zs = ZipfSong()
+        zs.addSong(0, 0, "")
+        self.assertEquals(zs.topQualitySongs(0), [])
+
+    def test_topQualitySongs_one_song(self):
+        zs = ZipfSong()
+        zs.addSong(0, 1, "Hi")
+        self.assertEquals(zs.topQualitySongs(1), [(0, 1, "Hi")])
 
 if __name__ == '__main__':
     unittest.main()
