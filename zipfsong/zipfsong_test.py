991781cd3b323188430b70543d884cfd6bb4a596
Victor Koronen <koronen@kth.se>
2013-02-03 17:03:11 +0100

    Add three song unit test

--- zipfsong/zipfsong_test.py
+++ zipfsong/zipfsong_test.py
@@ -19,6 +19,17 @@ class TestZipfSong(unittest.TestCase):
         zs.addSong(0, 1, "Hi")
         self.assertEquals(zs.topQualitySongs(1), [(0, 1, "Hi")])
 
+    def test_topQualitySongs_three_songs(self):
+        zs = ZipfSong()
+        zs.addSong(0, 10, "Foo")
+        zs.addSong(1, 7, "Bar")
+        zs.addSong(2, 5, "Baz")
+        self.assertEquals(zs.topQualitySongs(3), [
+            (2, 5, "Baz"),
+            (1, 7, "Bar"),
+            (0, 10, "Foo"),
+        ])
+
 if __name__ == '__main__':
     unittest.main()
 
