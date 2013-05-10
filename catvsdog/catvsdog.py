7bc974118ec3aab8a5011ad0e5196a8e646f8045
Victor Koronen <koronen@kth.se>
2013-05-10 02:13:49 +0200

    Remove old catvsdog implementation

--- catvsdog/catvsdog.py
+++ catvsdog/catvsdog.py
@@ -7,52 +7,12 @@ class CatVsDog:
         self.numberOfCats = c
         self.numberOfDogs = d
         self.numberOfVoters = v
-        self.angryVoters = {
-            'keep': {
-                'cats': [0 for i in range(c)],
-                'dogs': [0 for i in range(d)],
-            },
-            'throw': {
-                'cats': [0 for i in range(c)],
-                'dogs': [0 for i in range(d)],
-            },
-        }
 
     def addVote(self, keep, throw):
-        catsdogs = 'cats' if keep[0] == 'C' else 'dogs'
-        self.angryVoters['throw'][catsdogs][int(keep[1:])-1] += 1
-
-        catsdogs = 'cats' if throw[0] == 'C' else 'dogs'
-        self.angryVoters['keep'][catsdogs][int(throw[1:])-1] += 1
+        pass
 
     def maximumNumberOfSatisfiedVoters(self):
         maximumNumberOfSatisfiedVoters = 0
-
-        for tc in range(self.numberOfCats):
-            n = self.angryVoters['throw']['cats'][tc]
-            for c in range(self.numberOfCats):
-                if c != tc:
-                    n += self.angryVoters['keep']['cats'][c]
-            for d in range(self.numberOfDogs):
-                n += self.angryVoters['keep']['dogs'][d]
-
-            numberOfSatisfiedVoters = self.numberOfVoters - n/2
-
-            if numberOfSatisfiedVoters > maximumNumberOfSatisfiedVoters:
-                maximumNumberOfSatisfiedVoters = numberOfSatisfiedVoters
-
-        for td in range(self.numberOfDogs):
-            n = self.angryVoters['throw']['dogs'][td]
-            for c in range(self.numberOfCats):
-                n += self.angryVoters['keep']['cats'][c]
-            for d in range(self.numberOfDogs):
-                if d != td:
-                    n += self.angryVoters['keep']['dogs'][d]
-
-            numberOfSatisfiedVoters = self.numberOfVoters - n/2
-
-            if numberOfSatisfiedVoters > maximumNumberOfSatisfiedVoters:
-                maximumNumberOfSatisfiedVoters = numberOfSatisfiedVoters
 
         return maximumNumberOfSatisfiedVoters
 
